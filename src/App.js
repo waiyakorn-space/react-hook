1337f9f5a95ce28ed9853f90074640dcc5da8092
Waiyakorn Phoomjarownwat <waiyakorn.krow@gmail.com>
2021-06-06 00:42:17 +0700

    6062021

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{ useState } from 'react'
 
-function App() {
+
+const App = () =>{
+  const [count,setCount] = useState(0);
+
+  const handleIncrement = () => setCount(count+1)
+  const handleDecrement = () => setCount(count-1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Count: {count}
+      <br />
+
+      <button type="button" onClick={handleIncrement}>Increment</button>
+      <button type="button" onClick={handleDecrement}>Decrement</button>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
